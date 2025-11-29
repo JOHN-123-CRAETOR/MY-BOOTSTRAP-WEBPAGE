6f2f45fedc944f396c4f57416f75d5330ef17607
John Boateng <boatwan419@gmail.com>
2025-11-29 17:17:40 +0000

    Deployment

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/MY-BOOTSTRAP-WEBPAGE/'  // <- your repo name here
+  base: '/MY-BOOTSTRAP-WEBPAGE'  // <- your repo name here
 });
